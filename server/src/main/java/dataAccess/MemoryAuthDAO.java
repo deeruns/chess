d21df5bc1041eb7e46e45c0ba2dae7b0e883cca1
Davin Thompson <davindthomp@gmail.com>
2024-02-23 09:39:25 -0700

    MemoryAuthDAO updated

--- server/src/main/java/dataAccess/MemoryAuthDAO.java
+++ server/src/main/java/dataAccess/MemoryAuthDAO.java
@@ -1,22 +1,43 @@
 package dataAccess;
 
 import Models.AuthTokenData;
+import Models.UserData;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.UUID;
 
 public class MemoryAuthDAO implements AuthDAO {
     //CRUD operations
     private static HashMap<String, AuthTokenData> currAuth = new HashMap<>();
     //getauth
-//    public AuthTokenData getUser(String authToken) throws DataAccessException{
-//        if(!currAuth.containsKey(authToken)){
-//            throw new DataAccessException("invalid auth token");
-//        }
-//        return currAuth.get(authToken);
-//    }
-//    //createauths
-//    //removeauth
+    public AuthTokenData getUser(String authToken) throws DataAccessException{
+        if(!currAuth.containsKey(authToken)){
+            throw new DataAccessException("invalid auth token");
+        }
+        return currAuth.get(authToken);
+    }
+
+
+    public AuthTokenData createAuthToken(UserData user){
+        if (currAuth.containsKey(user.username())){
+            currAuth.remove(user.username());
+            //user only has one authToken
+        }
+        UUID auth = UUID.randomUUID();
+        return new AuthTokenData(auth.toString(), user.username());
+    }
+
+    public void createMemory(AuthTokenData authData) throws DataAccessException {
+        if (currAuth.containsKey(authData.authToken())){
+            throw new DataAccessException("Auth token already exists");
+        }
+
+        currAuth.put(authData.authToken(), authData);
+    }
+    public void deleteAuth(String authToken){
+        currAuth.remove(authToken);
+    }
     public void clear() throws DataAccessException{
         currAuth.clear();
     }
