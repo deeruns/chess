e4c2cd4314530bb610cf321277bc319c75552d8d
Davin Thompson <davindthomp@gmail.com>
2024-03-19 13:23:54 -0600

    server facade progress

--- client/src/main/java/serverFacade/ServerFacade.java
+++ client/src/main/java/serverFacade/ServerFacade.java
@@ -1,7 +1,21 @@
 package serverFacade;
 
 import ResponseException.ResponseException;
+import com.google.gson.Gson;
+import com.sun.net.httpserver.Request;
+import requests.CreateGameRequest;
+import requests.JoinGameRequest;
+import requests.LoginRequest;
+import requests.RegisterRequest;
+import response.CreateGameResponse;
+import response.ListGamesResponse;
+import response.ResponseRecord;
+import service.RegisterService;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
@@ -12,8 +26,30 @@ public class ServerFacade {
     public ServerFacade(String uRl) {
         servUrl = uRl;
     }
-    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws ResponseException{
-        try{
+    public void clear() throws ResponseException {
+        var path = "/db";
+    }
+    public ResponseRecord register(RegisterRequest request) throws ResponseException {
+        var path = "/session";
+    }
+    public ResponseRecord login(LoginRequest request) throws ResponseException {
+
+    }
+    public ListGamesResponse listGames(Request request) throws ResponseException {
+
+    }
+     public ResponseRecord logout(Request request) throws ResponseException{
+
+     }
+     public CreateGameResponse createGame(CreateGameRequest request) throws ResponseException{
+
+     }
+     public ResponseRecord joinGame(JoinGameRequest request)throws ResponseException{
+
+     }
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws ResponseException {
+        try {
             URL url = (new URI(servUrl + path)).toURL();
             HttpURLConnection http = (HttpURLConnection) url.openConnection();
             http.setRequestMethod(method);
@@ -21,14 +57,45 @@ public class ServerFacade {
 
             writeBody(request, http);
             http.connect();
-            if(http.getResponseCode() != 200){
-                throw new ResponseException(http.getResponseCode(), "Request Has Failed!");
-            }
-            readbody(http, responseClass);
+            throwIfNotSuccessful(http);
+            return readbody(http, responseClass);
 
-        }
-        catch (Exception exception) {
+        } catch (Exception exception) {
             throw new ResponseException(500, exception.getMessage());
         }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            throw new ResponseException(status, "failure: " + status);
+        }
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+
+    private void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String requestData = new Gson().toJson(request);
+            try (OutputStream requestBody = http.getOutputStream()) {
+                requestBody.write(requestData.getBytes());
+            }
+        }
+    }
+
+    private <T> T readbody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
     }
 }
