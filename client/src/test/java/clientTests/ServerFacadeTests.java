dce327db7f7a21fdafcb659839c785af127f8f06
Davin Thompson <davindthomp@gmail.com>
2024-03-21 14:23:52 -0600

    final commit part 2

--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -20,16 +20,16 @@ public class ServerFacadeTests {
 
     @BeforeAll
     public static void init() {
-//        server = new Server();
-//        var port = server.run(8080);
-//        System.out.println("Started test HTTP server on " + port);
-        facade = new ServerFacade("http://localhost:8080");
+        server = new Server();
+        var port = server.run(0);
+        System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("http://localhost:"+ port);
     }
 
-//    @AfterAll
-//    static void stopServer() {
-//        server.stop();
-//    }
+    @AfterAll
+    static void stopServer() {
+        server.stop();
+    }
     @AfterAll
     static void clearDB() throws DataAccessException {
         facade.clear();
